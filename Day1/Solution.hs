08f5476322c94244c1a607d16649e2837f50c451
Netanel Friedman <netanel.friedman6@gmail.com>
2023-12-08 15:22:04 +0200

    Add the other example for day 1

--- Day1/Solution.hs
+++ Day1/Solution.hs
@@ -4,7 +4,7 @@ module Day1.Solution (Day1(..)) where
 import Parts
 
 import Data.Char (isDigit)
-import Data.List (findIndex)
+import Data.List (findIndex, isPrefixOf)
 
 data Day1 = Day1 deriving Show
 
@@ -13,14 +13,8 @@ instance Part1 Day1 [String] where
   solve1 _ = Result . sum . map extractNumber
     where
       extractNumber :: String -> Int
-      extractNumber []     = undefined
+      extractNumber [] = 0
       extractNumber xs = read [head xs, last xs]
-
-startsWith :: Eq a => [a] -> [a] -> Bool
-startsWith [] [] = True
-startsWith [] _ = True
-startsWith _ [] = False
-startsWith (x:xs) (y:ys) = x == y && startsWith xs ys
 
 instance Part2 Day1 [String] where
   parse2 day = parse1 day . unlines . map go . lines
@@ -29,7 +23,7 @@ instance Part2 Day1 [String] where
       go [] = []
       go (digit:xs) | isDigit digit = digit : go xs
       go input =
-        case findIndex (`startsWith` input) names of
+        case findIndex (`isPrefixOf` input) names of
           Just i -> head (show i) : go (tail input)
           Nothing -> go (tail input)
 

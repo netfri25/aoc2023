73c1bf0b7cf5bb6212194b32f2e8c28c5c6be79c
Netanel Friedman <netanel.friedman6@gmail.com>
2023-12-09 12:53:58 +0200

    Don't overcomplicate parsing

--- Day9/Solution.hs
+++ Day9/Solution.hs
@@ -2,18 +2,11 @@
 module Day9.Solution (Day9(..)) where
 
 import Parts
-import Parser
-
-import Data.Maybe (fromJust)
-import Control.Monad (mfilter)
 
 data Day9 = Day9 deriving Show
 
-parseNumList :: Parser String [Int]
-parseNumList = mfilter (not . null) $ sepBy (eqP ' ') numP
-
 instance Part1 Day9 [[Int]] where
-  parse1 _ = fst . fromJust . runParserT (sepBy (eqP '\n') parseNumList)
+  parse1 _ = map (map read . words) . lines
   solve1 _ = Result . sum . map predict
 
 predict :: [Int] -> Int

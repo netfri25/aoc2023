eabb40f4576df11b18c3440f163b9cbf43d7e182
Netanel Friedman <netanel.friedman6@gmail.com>
2023-12-10 23:07:30 +0200

    The shittiest code ever, but I finally finished day 10 part 2 without cheating!!!! (not that I cheated on any of the other days, but at some point I will probably give up)

--- Day10/Solution.hs
+++ Day10/Solution.hs
@@ -5,7 +5,8 @@ import Parts
 
 import qualified Data.Map as M
 import Data.List (find)
-import Data.Maybe (catMaybes, mapMaybe, listToMaybe)
+import Data.Maybe (catMaybes, mapMaybe, listToMaybe, fromJust, fromMaybe)
+import Data.Bits (xor)
 
 data Day10 = Day10 deriving Show
 
@@ -92,8 +93,61 @@ mainLoop pipes = listToMaybe $ mapMaybe (stepToStart start_pos pipes <$> initial
 
 instance Part1 Day10 (Grid Pipe) where
   parse1 _ = M.fromList . parsePipes
-  solve1 _ = show . fmap (flip div 2 . M.size) . mainLoop
+  solve1 _ = show . fmap (flip div 2 . M.size ) . mainLoop
 
-instance Part2 Day10 (Grid Pipe) where
-  parse2 = parse1
-  solve2 _ = show . const ()
+replaceStart :: Grid Pipe -> Grid Pipe
+replaceStart pipes = M.insert start_pos start' pipes
+  where
+    start_pos = fst $ fromJust $ find ((==) Start . snd) $ M.assocs pipes
+    start' = Pipe $ filter (connectedTo pipes start_pos . (start_pos +)) [up, down, left, right]
+
+showPipe :: Pipe -> Maybe Char
+showPipe = flip lookup
+  [ (Pipe [up, down],'|')
+  , (Pipe [left, right],'-')
+  , (Pipe [up, right],'L')
+  , (Pipe [left, up],'J')
+  , (Pipe [left, down],'7')
+  , (Pipe [down, right],'F')
+  ]
+
+showPipesGrid :: Grid Pipe -> [String]
+showPipesGrid pipes = do
+  y <- [min_y..max_y]
+  return $ do
+    x <- [min_x..max_x]
+    let pos = Position x y
+    return $ fromMaybe ' ' $ M.lookup pos pipes >>= showPipe
+  where
+    min_x = minimum $ map posX $ M.keys pipes
+    min_y = minimum $ map posY $ M.keys pipes
+    max_x = maximum $ map posX $ M.keys pipes
+    max_y = maximum $ map posY $ M.keys pipes
+
+countInside :: String -> Int
+countInside = countInside' False []
+  where
+    countInside' :: Bool -> [Char] -> String -> Int
+    countInside' _ _ [] = 0
+    countInside' inside stack (c:cs) =
+      case c of
+        '|' -> countInside' (not inside) stack cs
+        '-' -> countInside' inside stack cs
+        'L' -> countInside' inside (c:stack) cs
+        'F' -> countInside' inside (c:stack) cs
+        '7' ->
+          let inside' = (head stack == 'L') `xor` inside
+           in countInside' inside' (tail stack) cs
+        'J' ->
+          let inside' = (head stack == 'F') `xor` inside
+           in countInside' inside' (tail stack) cs
+        ' ' -> fromEnum inside + countInside' inside stack cs
+        _ -> undefined
+
+instance Part2 Day10 [String] where
+  parse2 day input = showPipesGrid $ fromJust main_pipes
+    where
+      pipes = parse1 day input
+      main_loop = mainLoop pipes
+      main_pipes = replaceStart . M.intersectionWith const pipes <$> main_loop
+  solve2 _ = show . sum . map countInside
